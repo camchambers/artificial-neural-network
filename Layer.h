0ecaf497625f5d54f84bacab5583bff0c1e7efca
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-20 13:03:35 -0400

    Created a header file for the Layer class

---
+++ Layer.h
@@ -0,0 +1,36 @@
+using namespace std;
+
+#include <vector>
+#include "Connection.h"
+#include "Neuron.h"
+
+#pragma once
+
+class Layer
+{
+public:
+    unsigned layerIndex;
+    vector<Neuron> neurons;
+
+    /**
+     * @brief Construct a new Layer object
+     * 
+     * @param numberOfNeurons The number of Neurons the layer will contain
+     * @param index The index of the layer being created
+     * @param numberOfOutputs The number of output Connections that the Neurons
+     * in the Layer will contain
+     */
+    Layer(unsigned numberOfNeurons, unsigned index, unsigned numberOfOutputs);
+    
+    /**
+     * @brief Prints the number of neurons in the layer
+     * 
+     */
+    unsigned neuronCount();
+
+    /**
+     * @brief Prints all of the Neurons of a Layer
+     * 
+     */
+    void print();
+};
