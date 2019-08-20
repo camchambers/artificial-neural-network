288fab4f7a3f0cdeaa5f6c6e17cc171e54dc7ecc
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-20 13:03:47 -0400

    Created a header file for the Neuron class

---
+++ Neuron.h
@@ -0,0 +1,43 @@
+using namespace std;
+
+#include <vector>
+#include "Connection.h"
+
+#pragma once
+
+/**
+ * @brief Neurons are the building blocks of an Artificial Neural Network and 
+ * drive the input to specific output Neurons according to the values of connection
+ * weights between neurons
+ * 
+ */
+class Neuron
+{
+public:
+    /**
+     * @brief Construct a new Neuron object
+     * 
+     * @param numberOfOutputConnections The number of the connections in the 
+     * following layer. Since the final layer contains the output Neurons to the 
+     * Artificial Neural Network, they will not contain output connections
+     */
+    Neuron(unsigned numberOfOutputConnections);
+
+    /**
+     * @brief Sets the output value of the current Neuron
+     * 
+     * @param value The output value to set for the current neuron
+     */
+    void setOutputValue(double value);
+
+    /**
+     * @brief Prints all of the Connections of a Neuron
+     * 
+     * @param neuronIndex 
+     */
+    void print(unsigned neuronIndex);
+
+private:
+    vector<Connection> connections;
+    double outputValue = 0;
+};
