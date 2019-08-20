8ffe039ae742f97db912d3933d0a75359beca7af
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-20 14:41:18 -0400

    Removed forward references for the Layer class from Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,35 +6,6 @@ using namespace std;
 #include "Neuron.h"
 #include "Layer.h"
 
-Layer::Layer(unsigned numberOfNeurons, unsigned index, unsigned numberOfOutputs)
-{
-    // Assign layer index
-    layerIndex = index;
-
-    // Populate the Layer with Neurons based on the numberOfNeurons parameter
-    for (unsigned neuronIndex = 0; neuronIndex < numberOfNeurons; ++neuronIndex)
-    {
-        cout << "Adding neuron " << (neuronIndex + 1) << " to layer " << layerIndex + 1 << "." << endl;
-        neurons.push_back(Neuron(numberOfOutputs));
-    }
-}
-
-unsigned Layer::neuronCount()
-{
-    return neurons.size();
-}
-
-void Layer::print()
-{
-    unsigned neuronIndex = 0;
-    for (neuronIndex = 0; neuronIndex < neurons.size(); ++neuronIndex)
-    {
-        neurons[neuronIndex].print(neuronIndex++);
-    }
-}
-}
-;
-
 class ArtificialNeuralNetwork
 {
 public:
