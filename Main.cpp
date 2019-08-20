f8dbe133f10d0e2096f17e2c2870f63a52129a86
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-20 14:40:46 -0400

    Removed forward references for the Neuron class from Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,36 +6,6 @@ using namespace std;
 #include "Neuron.h"
 #include "Layer.h"
 
-Neuron::Neuron(unsigned numberOfOutputConnections)
-{
-    // Create connections for each neuron
-    if (numberOfOutputConnections > 0)
-    {
-        for (unsigned connectionIndex = 0; connectionIndex < numberOfOutputConnections; ++connectionIndex)
-        {
-            cout << "Adding connection " << (connectionIndex + 1) << " with " << numberOfOutputConnections << " outputs" << endl;
-            connections.push_back(Connection());
-        }
-    }
-}
-
-void Neuron::setOutputValue(double value)
-{
-    outputValue = value;
-}
-
-void Neuron::print(unsigned neuronIndex)
-{
-    cout << "\tNeuron " << neuronIndex + 1 << " ";
-    for (auto connection : connections)
-    {
-        cout << "(" << outputValue << ",";
-        connection.print();
-        cout << ")";
-    }
-    cout << endl;
-}
-
 Layer::Layer(unsigned numberOfNeurons, unsigned index, unsigned numberOfOutputs)
 {
     // Assign layer index
