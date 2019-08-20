6857e83bae770570bcd0b5d05eaacb051aadde6d
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-20 14:40:12 -0400

    Removed forward references for the Connection class from Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,19 +5,6 @@ using namespace std;
 #include "Connection.h"
 #include "Neuron.h"
 #include "Layer.h"
-#include "ArtificialNeuralNetwork.h"
-
-// TODO Separate classes into individual files
-
-Connection::Connection()
-{
-    weight = (double)rand() / RAND_MAX * 2.0 - 1.0;
-}
-
-void Connection::print()
-{
-    cout << weight;
-}
 
 Neuron::Neuron(unsigned numberOfOutputConnections)
 {
@@ -69,96 +56,122 @@ unsigned Layer::neuronCount()
 
 void Layer::print()
 {
-    for (unsigned neuronIndex = 0; neuronIndex < neurons.size(); ++neuronIndex)
+    unsigned neuronIndex = 0;
+    for (neuronIndex = 0; neuronIndex < neurons.size(); ++neuronIndex)
     {
         neurons[neuronIndex].print(neuronIndex++);
     }
 }
+}
+;
 
-ArtificialNeuralNetwork::ArtificialNeuralNetwork(const vector<unsigned> &topology)
+class ArtificialNeuralNetwork
 {
-    // Print the topology of the Artificial Neural Network
-    cout << "Creating a neural network with a ";
-    for (unsigned layerIndex = 0; layerIndex < topology.size(); ++layerIndex)
+public:
+    /**
+     * @brief Construct a new Artificial Neural Network object
+     * 
+     * @param topology The topology (number of Neurons in each layer) of the neural network
+     */
+    ArtificialNeuralNetwork(const vector<unsigned> &topology)
     {
-        cout << topology[layerIndex];
-        if (layerIndex < (topology.size() - 1))
+        // Print the topology of the Artificial Neural Network
+        cout << "Creating a neural network with a ";
+        for (unsigned layerIndex = 0; layerIndex < topology.size(); ++layerIndex)
         {
-            cout << ":";
+            cout << topology[layerIndex];
+            if (layerIndex < (topology.size() - 1))
+            {
+                cout << ":";
+            }
         }
-    }
-    cout << " toplogy." << endl;
+        cout << " toplogy." << endl;
 
-    // Number of connections to the next layer
-    unsigned int numberOfOutputs = 0;
+        // Number of connections to the next layer
+        unsigned int numberOfOutputs = 0;
 
-    // Create neural network layers
-    for (unsigned layerIndex = 0; layerIndex < topology.size(); ++layerIndex)
+        // Create neural network layers
+        for (unsigned layerIndex = 0; layerIndex < topology.size(); ++layerIndex)
+        {
+            unsigned layerSize = topology[layerIndex];
+
+            cout << endl
+                 << "Adding a " << layerSize << " neuron layer to the neural network."
+                 << endl;
+
+            // Pass in the number of connections for all Neurons in the layer to be constructed
+            // The last layer will not contain any connections, since it is an output layer
+            numberOfOutputs = layerIndex < (topology.size() - 1) ? topology[layerIndex + 1] : 0;
+
+            layers.push_back(Layer(layerSize, layerIndex, numberOfOutputs));
+        }
+    };
+
+    /**
+     * @brief Prints all of the layers of an Artificial Neural Network
+     * 
+     */
+    void print()
     {
-        unsigned layerSize = topology[layerIndex];
-
         cout << endl
-             << "Adding a " << layerSize << " neuron layer to the neural network."
+             << "Printing neural network:" << endl
              << endl;
 
-        // Pass in the number of connections for all Neurons in the layer to be constructed
-        // The last layer will not contain any connections, since it is an output layer
-        numberOfOutputs = layerIndex < (topology.size() - 1) ? topology[layerIndex + 1] : 0;
-
-        layers.push_back(Layer(layerSize, layerIndex, numberOfOutputs));
-    }
-};
-
-void ArtificialNeuralNetwork::print()
-{
-    cout << endl
-         << "Printing neural network:" << endl
-         << endl;
-
-    for (unsigned layerIndex = 0; layerIndex < layers.size(); ++layerIndex)
-    {
-        cout << "Layer " << (layerIndex + 1) << endl;
-        layers[layerIndex].print();
+        for (unsigned layerIndex = 0; layerIndex < layers.size(); ++layerIndex)
+        {
+            cout << "Layer " << (layerIndex + 1) << endl;
+            layers[layerIndex].print();
+            cout << endl;
+        }
         cout << endl;
-    }
-    cout << endl;
-};
+    };
 
-void ArtificialNeuralNetwork::train(const vector<double> inputVals)
-{
-    cout << "Training Neural Network" << endl;
-
-    unsigned inputLayerSize = layers[0].neuronCount();
-
-    // Validate that the rows of the input vector to the Neural Network matches
-    // the number of Neurons in the first layer (input Neurons) of the Neural Network
-    if (inputVals.size() != inputLayerSize)
+    /**
+     * @brief Trains an Artificial Neural Network
+     * 
+     * @param inputVals The input values to the neural network. 
+     *  The number of rows in the input value vector must equal
+     *  the number of input Neuron in the neural network. 
+     */
+    void train(const vector<double> inputVals)
     {
-        cout << endl
-             << "Error: The number of input values does not match the number of input neurons in the Neural Network." << endl
-             << endl;
-        exit(1);
-    }
+        cout << "Training Neural Network" << endl;
 
-    // Assign (latch) input values to the Neurons of the input Layer
-    for (unsigned i = 0; i < inputLayerSize; i++)
-    {
-        layers[0].neurons[i].setOutputValue(inputVals[i]);
-    }
+        unsigned inputLayerSize = layers[0].neuronCount();
 
-    // Forward propogate input values
-    // Skip the input layer because the output values for the input layer
-    // have already been set
-    for (unsigned layerIndex = 1; layerIndex < layers.size(); ++layerIndex)
-    {
-        cout << endl
-             << "Layer " << layerIndex << endl;
-        for (unsigned neuronIndex = 0; neuronIndex < layers[layerIndex].neuronCount(); ++neuronIndex)
+        // Validate that the rows of the input vector to the Neural Network matches
+        // the number of Neurons in the first layer (input Neurons) of the Neural Network
+        if (inputVals.size() != inputLayerSize)
         {
             cout << endl
-                 << "\tNeuron " << neuronIndex << endl;
+                 << "Error: The number of input values does not match the number of input neurons in the Neural Network." << endl
+                 << endl;
+            exit(1);
         }
-    }
+
+        // Assign (latch) input values to the Neurons of the input Layer
+        for (unsigned i = 0; i < inputLayerSize; i++)
+        {
+            layers[0].neurons[i].setOutputValue(inputVals[i]);
+        }
+
+        // Forward propogate input values
+        // Skip the input layer because the output values for the input layer
+        // have already been set
+        for (unsigned layerIndex = 1; layerIndex < layers.size(); ++layerIndex)
+        {
+            cout << endl
+                 << "Layer " << layerIndex << endl;
+            for (unsigned neuronIndex = 0; neuronIndex < layers[layerIndex].neuronCount(); ++neuronIndex)
+            {
+                cout << endl
+                     << "\tNeuron " << neuronIndex << endl;
+            }
+        }
+    };
+
+private:
+    vector<Layer> layers;
 };
 
 int main()
