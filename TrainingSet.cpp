1fdc94a3db3b9b1c18ca17ed0c2dbb198b2ce71a
Cameron Chambers <cameronchambers@trentu.ca>
2019-08-28 16:23:31 -0400

    Created a training set class

---
+++ TrainingSet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "TrainingSet.h"
+
+using namespace std;
+
+void TrainingSet::read(string fileName)
+{
+    cout << "Attempting to read traning set from file \"" << fileName << "\"" << endl
+         << endl;
+}
