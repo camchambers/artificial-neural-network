755f3e109614db3c67b09198e7e7cd0863121218
Cameron Chambers <cameronchambers@trentu.ca>
2019-09-04 16:32:02 -0400

    Added code comments to the TrainingSet header file

--- TrainingSet.h
+++ TrainingSet.h
@@ -5,14 +5,50 @@ using namespace std;
 class TrainingSet
 {
 public:
+    /**
+     * @brief Reads a training set from a CSV file into a vector of instances
+     * 
+     * @param filename the relative path to the csv file to be read
+     */
     void read(string filename);
+
+    /**
+     * @brief The number of records in the CSV file that has been read
+     * 
+     * @return int 
+     */
     int recordCount();
+
+    /**
+     * @brief Gets a particular record from the training set given an index
+     * 
+     * @param index The index of the record to read
+     * @return vector<double> 
+     */
     vector<double> getRecord(int index);
+
+    /**
+     * @brief Scales training data to ensure attributes are weighted equally
+     * 
+     */
     void scale();
 
 private:
+    /**
+     * @brief The parsed training data read from the CSV file
+     * 
+     */
     vector<vector<double>> trainingData;
+
+    /**
+     * @brief Parsed class labels read from the CSV file
+     * 
+     */
     vector<int> classLabels;
-    int readIndex = 0;
+
+    /**
+     * @brief The number of records read from the training set file
+     * 
+     */
     int numberOfRecords;
 };
