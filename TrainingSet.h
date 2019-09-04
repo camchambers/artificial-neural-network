d3df4df14997f1cc2135277124e41694a1dfef98
Cameron Chambers <cameronchambers@trentu.ca>
2019-09-04 16:08:36 -0400

    Added recordCount and getRecord declarations to the trainingSet header file

--- TrainingSet.h
+++ TrainingSet.h
@@ -6,9 +6,13 @@ class TrainingSet
 {
 public:
     void read(string filename);
+    int recordCount();
+    vector<double> getRecord(int index);
     void scale();
 
 private:
     vector<vector<double>> trainingData;
     vector<int> classLabels;
+    int readIndex = 0;
+    int numberOfRecords;
 };
