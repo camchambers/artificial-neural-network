36071d9c8b357e3cf539ab96d29262fe7c8ed478
Cameron Chambers <cameronchambers@trentu.ca>
2019-09-04 16:12:03 -0400

    Added a recordCount function to the TrainingSet class

--- TrainingSet.cpp
+++ TrainingSet.cpp
@@ -3,11 +3,17 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include "TrainingSet.h"
 
 using namespace std;
 
+// Return the number of records read by the training set
+int TrainingSet::recordCount()
+{
+    return this->numberOfRecords;
+}
+
+// Reads a file to create a training set
 void TrainingSet::read(string fileName)
 {
     cout << "Attempting to read traning set from file \"" << fileName << "\"" << endl
